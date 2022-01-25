5c29e60a1dfbb1c9a5ed9c4f0c0d847d4676ed7d
Yamsunsee <yamsunsee@gmail.com>
2022-01-25 18:41:19 +0700

    Edit menu icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,31 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./*.html", "./src/**/*.js"],
-  content: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        primary: ["Montserrat", "sans-serif"],
-        secondary: ["DM Sans", "sans-serif"],
-      },
-      colors: {
-        primary: "#0B0547",
-        lightBlue: "#3975F2",
-        darkBlue: "#0643C3",
-        darkestBlue: "#07032F",
-        orange: "#EEA153"
-      },
-      gridTemplateColumns: {
-        testimonial: "4rem 1fr 1fr 4rem",
-        header: "1fr 3fr 1fr",
-        footer: "3fr 1fr 1fr 1fr"
-      },
-      screens: {
-        "xs": "320px",
-        "sm": "512px",
-        "md": "768px",
-        "lg": "1024px",
-        "xl": "1200px"
-      }
-    },
-  },
-  plugins: [],
+	mode: "jit",
+	purge: ["./*.html", "./src/**/*.js"],
+	theme: {
+		extend: {
+			fontFamily: {
+				primary: ["Montserrat", "sans-serif"],
+				secondary: ["DM Sans", "sans-serif"],
+			},
+			colors: {
+				primary: "#0B0547",
+				lightBlue: "#3975F2",
+				darkBlue: "#0643C3",
+				darkestBlue: "#07032F",
+				orange: "#EEA153",
+			},
+			gridTemplateColumns: {
+				testimonial: "4rem 1fr 1fr 4rem",
+				header: "1fr 3fr 1fr",
+				footer: "3fr 1fr 1fr 1fr",
+			},
+			screens: {
+				xs: "320px",
+				sm: "512px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1200px",
+			},
+		},
+	},
 }
